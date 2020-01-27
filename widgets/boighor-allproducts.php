e353aec325cc90880323dbe14a1b1e81482d99cf
Muradcmt85 <42121306+Muradcmt85@users.noreply.github.com>
2020-01-27 13:32:18 +0600

    Boighor All products

--- widgets/boighor-allproducts.php
+++ widgets/boighor-allproducts.php
@@ -171,9 +171,9 @@ class Allproducts_section extends \Elementor\Widget_Base {
 											<div class="action">
 												<div class="actions_inner">
 													<ul class="add_to_links">
-														<li><a class="cart" href="cart.html"><i class="bi bi-shopping-bag4"></i></a></li>
-														<li><a class="wishlist" href="wishlist.html"><i class="bi bi-shopping-cart-full"></i></a></li>
-														<li><a class="compare" href="#"><i class="bi bi-heart-beat"></i></a></li>
+														<li><a class="cart" href="<?php echo get_the_permalink()?>"><i class="bi bi-shopping-bag4"></i></a></li>
+														<li><a class="wishlist" href="<?php echo get_the_permalink()?>"><i class="bi bi-shopping-cart-full"></i></a></li>
+														<li><a class="compare" href="<?php echo get_the_permalink()?>"><i class="bi bi-heart-beat"></i></a></li>
 														<li><a data-toggle="modal" title="Quick View" class="quickview modal-view detail-link" href="#productmodal"><i class="bi bi-search"></i></a></li>
 													</ul>
 												</div>
